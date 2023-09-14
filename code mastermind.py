bca3103a6da83a1bf1a10537e6832611157d0590
Hyaenidae666 <128646492+Hyaenidae666@users.noreply.github.com>
2023-09-14 16:40:54 +0200

    Update code mastermind.py

--- code mastermind.py
+++ code mastermind.py
@@ -1,4 +1,6 @@
 import random
+import matplotlib.pyplot as plt
+import numpy as np
 
 #creer un code, check la reponse, afficher les indices
 
@@ -14,25 +16,25 @@ def gen_code(size,color):  #generation de code de taille:"size" et de nombre de 
 
 def guess_check(secret_code,guess):
     good=0       #bien placé/mal placé
-    bad=0          
+    bad=0
     newcode=secret_code.copy()
-    for i in range(len(secret_code)):
+    for i in range(0,len(secret_code)):
         if guess[i]==secret_code[i]:
-            good +=1
+            good = good +1
             if newcode[i]==1:
-                bad -=1
+                bad = bad -1
                 for j in range(i+1,len(secret_code)):
                     if guess[i]==newcode[j]:
-                        bad +=1
-                        break
+                        bad = bad +1
                         newcode[j]=1
+                        break
             newcode[i]=0
         else:
-            for j in range(len(secret_code)):
-                if guess[i]==newcode[j] :
-                    bad +=1
-                    break
+            for j in range(0,len(secret_code)):
+                if guess[i]==newcode[j]:
+                    bad = bad +1
                     newcode[j]=1
+                    break
     return (good,bad)
 
 #print(guess_check(['H', 'E', 'G', 'E'],['E','E','G','G']))
@@ -63,8 +65,8 @@ def jouer(a,b,c):
 def all_code0(size,color):
     L=[]
     return all_code1(size,color,L)
-    
-    
+
+
 def all_code1(size,color,L):
     size=size-1
     M=[]
@@ -74,21 +76,21 @@ def all_code1(size,color,L):
         for i in range(0,color):
             all_code1(size,color,L)'''
 
-def all_code(color): ###pb de size
+'''def all_code(size,color): ###pb de size
     L=[]
     for i in range(0,color):
         for j in range(0,color):
             for f in range(0,color):
                 for h in range(0,color):
                     L.append([C[i],C[j],C[f],C[h]])
-    return L
-            
+    return L'''
 
-def efficacity_test(size,color,prog): ###pb de longueur des tours max
-    allcode=all_code(color)
-    L=[0]*10
+
+def efficacity_test_grosbg(size,color,List): ###pb de longueur des tours max
+    allcode=List(size,color)
+    L=[0]*12
     for i in range(0,len(allcode)):
-        turn=grosbg(size,color,allcode[i],allcode)
+        turn=grosbg(allcode[i],allcode)
         L[turn-1]+=1
     total=0
     moy=0
@@ -98,6 +100,8 @@ def efficacity_test(size,color,prog): ###pb de longueur des tours max
     moy=moy/total
     return (L,total,"moy:",moy)
 
+
+
 ##################################################################################
 '''Premier programme de résolution, choix aléatoire parmis les codes plausibles'''
 ##################################################################################
@@ -105,15 +109,15 @@ def efficacity_test(size,color,prog): ###pb de longueur des tours max
 def grosbg0(size,color):
     secret_code=gen_code(size,color)
     print(secret_code)
-    return grosbg(size,color,secret_code,all_code(color))
+    return grosbg(secret_code,all_code(size,color))
 
- 
-def grosbg(size,color,secret_code,all_code):
+
+def grosbg(secret_code,all_code): ###size pour first nn defini
     L=all_code.copy()
     first=L[0]
     M=[]
     a=1
-    while guess_check(secret_code,first) != (size,0):
+    while guess_check(secret_code,first) != (len(first),0):
 #        print(first)
 #        print(guess_check(secret_code,first))
         for i in range(0,len(L)):
@@ -125,45 +129,78 @@ def grosbg(size,color,secret_code,all_code):
         a=a+1
     return a
 
+
+
 ###########################################################################
 '''Deuxième programme de résolution en utilisant une stratégie gloutonne'''
 ###########################################################################
 
 #on peut réutiliser grosbg mais en triant allcode selon l'entropie, le programme va donc choisir le code le plus entropique
 
-
-def tri_entropie(L):
-    if len(L)==1:
-        return L
-    else:
-        n=len(L)//2
-        l1=L[:n]
-        l2=L[n:]
-        return fusion(tri_entropie(l1),tri_entropie(l2))
-        
-def fusion(l1,l2):
-    n=len(l1)+len(l2)
-    i=0
-    j=0
-    L=[]
-    for h in range(n):
-        if len(l1)==i:
-            L.append(l2[j])
-            j+=1
-        elif len(l2)==j:
-            L.append(l1[i])
-            i+=1
-        elif l1[i]<l2[j]:
-            L.append(l1[i])
-            i+=1
-        else:
-            L.append(l2[j])
-            j+=1
-    return L
+def tri_entropy(size,color):
+    code=all_code(size,color)
+    C=[[] for i in range(size)]
+    for i in range(len(code)):
+        A=[]
+        for j in range(size):
+            if code[i][j] not in A:
+                A.append(code[i][j])
+        a=len(A)
+        C[size-a].append(code[i])
+    return [item for sublist in C for item in sublist]
 
 
 
+###########################################################
+'''Affichage des résultats des programmes dans un graphe'''
+###########################################################
 
+
+cycle_color=('#FF0000','#EF00FF','#2B00FF','#00E6FF','#00FF2B','#FFFF00','#FF0000','#EF00FF','#2B00FF','#00E6FF','#00FF2B','#FFFF00')
+
+def affichage_color_simple():
+    x=[i for i in range(1,13)]
+    lab=[]
+    for i in range(1,7):
+        result=efficacity_test_grosbg(4,i,all_code)
+        lab.append(str(i)+" colors code, moy: "+str(round(result[-1],2)))
+        plt.plot(x,np.array(result[0])/result[1]*100,color=cycle_color[i-1],marker='+')
+        plt.legend(lab)
+    plt.xlabel('Number of guesses')
+    plt.ylabel('%')
+    plt.title('simple strategy')
+    plt.show()
+    return None
+
+def affichage_color_entropy():
+    x=[i for i in range(1,13)]
+    lab=[]
+    for i in range(1,11):
+        result=efficacity_test_grosbg(4,i,tri_entropy)
+        lab.append(str(i)+" colors code, moy: "+str(round(result[-1],2)))
+        plt.plot(x,np.array(result[0])/result[1]*100,color=cycle_color[i-1],marker='+')
+        plt.legend(lab)
+    plt.xlabel('Number of guesses')
+    plt.ylabel('%')
+    plt.title('entropy strategy')
+    plt.show()
+    return None
+
+
+def all_code(size,color):
+    m=[[] for i in range(color**size)]
+    return new_all_code(size,color,m)
+
+def new_all_code(size,color,m):
+    a=0
+    n=len(m)
+    b=n//size
+    while a<n:
+        for i in range(color):
+            for j in range(a,b):
+                m[j].append(C[i])
+        a,b=b,b+a
+    return new_all_code(size-1,color,m)
 
 
 
